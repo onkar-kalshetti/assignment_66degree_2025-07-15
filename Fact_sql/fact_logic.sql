9b434a5f54cdb9781dff8189c0b2c228a8591ed6
“onkar” <gouri@osplabs.com>
2025-07-16 13:06:42 +0530

    adding changes

--- Fact_sql/fact_logic.sql
+++ Fact_sql/fact_logic.sql
@@ -23,5 +23,5 @@ SELECT
     s.rating
 
 FROM stage_sales s
-JOIN dim_customer c ON s.customer_id = c.customer_id
-JOIN dim_product p ON s.product_id = p.product_id;
+LEFT JOIN dim_customer c ON s.customer_id = c.customer_id
+LEFT JOIN dim_product p ON s.product_id = p.product_id;
